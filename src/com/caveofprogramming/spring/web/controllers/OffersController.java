c08defa3bfbc596e327c5eeaeaaea1ac6f81d1a5
Akbar Hirani <akbar.hirani123@gmail.com>
2016-04-18 12:59:17 -0400

    Many aspects of the project are complete
    Topics covered and notes in books:
    04/12-04/18
    - A Basic Non-Spring Web App (Dynamic web project)
    -Bringing in Maven (Convert to Maven)
    - The Dispatcher Servlet (Add the controller servlet)
    -Adding a controller (Connect the controller)
    -View Resolver (Add link to web.xml)
    -Adding Data to the Session (Passing data to jsp)
    -Using Spring Data Models (Passing data to jsp)
    -Using JSTL (Passing data to jsp)
    -Configuring JNDI (Configuring MYSQL Data Source)
    -Bringing in the DAO (Bringing in the Offers stuff)
    -Loading Bean Container with ContextLoaderListener (Used to load extra
    XML files)
    -Creating a DataSource Bean (making @Autowired work)
    -Adding a Service Layer (Add a layer to do functions performed by
    OffersDAO)
    -Adding a new controller (Create more controllers and jsps to make it
    cleaner code)

---
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,42 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.service.OffersService;
+
+@Controller
+public class OffersController {
+
+	private OffersService offersService;
+
+	@Autowired
+	public void setOffersService(OffersService offersService) {
+		this.offersService = offersService;
+	}
+
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
+
+		List<Offer> offers = offersService.getCurrent();
+
+		model.addAttribute("offers", offers);
+		
+		return "offers";
+	}
+
+	@RequestMapping("/createoffer")
+	public String createOffer(Model model) {
+
+		return "createoffer";
+	}
+}
